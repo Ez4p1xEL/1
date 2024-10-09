3e755284ce18e4f217a37c139adfe68b76fd1b57
Ez4p1xEL <rollandhuang914@gmail.com>
2024-10-10 00:28:00 +0800

    Pro update

---
+++ core/src/main/lmk.java
@@ -0,0 +1,11 @@
+package core.src.main;
+
+public class lmk {
+
+    // It does not have maven
+    // I can import nothing
+    public void debug(String message) {
+        System.out.println(message);
+    }
+
+}
